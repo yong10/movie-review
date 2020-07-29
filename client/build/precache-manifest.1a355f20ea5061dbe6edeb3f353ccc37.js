d9ca13e4e6ddb76120f05096e47e4ca932c02f30
unknown <yh650766@gmail.com>
2020-07-29 15:07:48 -0400

    added build

---
+++ client/build/precache-manifest.1a355f20ea5061dbe6edeb3f353ccc37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3fd03f79ad3465653e4091894092fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c12929dd3a6c27e3ca96",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7cf89deae70c7b059eaa",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "c12929dd3a6c27e3ca96",
+    "url": "/static/js/2.d000f53a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d000f53a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf89deae70c7b059eaa",
+    "url": "/static/js/main.8175d584.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
